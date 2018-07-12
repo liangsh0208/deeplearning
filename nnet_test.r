c7c382fa362520332042fd69c3058799e00fa85a
ShiHao Liang <liangshihao_0208@163.com>
2018-07-12 18:10:40 +0800

    Create nnet_test.r

---
+++ nnet_test.r
@@ -0,0 +1,24 @@
+library(nnet)
+library(mlbench)
+set.seed(1)
+ir<- rbind(iris3[,,1],iris3[,,2],iris3[,,3])
+targets<- class.ind(c(rep("s",50),rep("c",50),rep("v",50)))
+samp<- c(sample(1:50,25),sample(51:100,25),sample(101:150,25))
+
+ir1<- nnet(ir[samp,],
+           targets[samp,],
+           size = 2,
+           rang = 0.1,
+           softmax = F,
+           censored = T,
+           decay = 5e-4,
+           maxit = 200
+           )
+
+test.cl<- function(true,pred){
+  true<- max.col(true)
+  cres<- max.col(pred)
+  table(true,cres)
+}
+
+test.cl(targets[-samp,],predict(ir1,ir[-samp,]))
