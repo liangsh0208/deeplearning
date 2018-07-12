933d08d323fa47d0d591335fce801ce5c080158f
ShiHao Liang <liangshihao_0208@163.com>
2018-07-12 18:13:56 +0800

    Update nnet_test.r

--- nnet_test.r
+++ nnet_test.r
@@ -22,3 +22,14 @@ test.cl<- function(true,pred){
 }
 
 test.cl(targets[-samp,],predict(ir1,ir[-samp,]))
+
+data("iris")
+set.seed(2)
+ind = sample(2,nrow(iris),replace = TRUE,prob = c(0.7,0.3))
+trainset = iris[ind == 1,]
+testset = iris[ind == 2,]
+iris.nn = nnet(Species ~ .,data = trainset,size = 2,rang = 0.1,decay = 5e-4,maxit = 200)
+summary(iris.nn)
+iris.predict = predict(iris.nn,testset,type = "class")
+nn.table = table(testset$Species,iris.predict)
+nn.table
